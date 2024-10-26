659d31c57116b5aae82c4b95fa0eeba46b00f3ba
adarsh-iiit <adarshtech251@gmail.com>
2024-10-26 22:00:56 +0530

    aaaa

---
+++ script.js
@@ -0,0 +1,13 @@
+HOW_TO_USE()
+
+
+
+let element = document.getElementById("aaa");
+const currentwidth = window.innerWidth; // always pass window.innerWidth in currentWidth argument
+const nestedArray = [
+    [0, 300, 10, 20],
+    [400, 900, 50, 40],
+    [1000, 1200, 55, 65],
+];
+
+SET_FONT_SIZE(element, currentwidth, nestedArray); // call this function to set font size
